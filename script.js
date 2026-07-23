1f7e9c63403e37970905e6b25815782f3c1bae2d
JoseAndresBruh <joseandres.bruh@gmail.com>
2026-07-23 01:34:22 -0500

    Change API URL from HTTP to HTTPS

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ let cursosDB = [
 // ========================================
 // 3. MÉTODOS DE LA API (CONEXIÓN REAL A FASTAPI)
 // ========================================
-const API_URL = "http://100.53.57.10:8000/api/cursos";
+const API_URL = "https://100.53.57.10:8000/api/cursos";
 
 // GET: Obtener todos los cursos
 async function apiGetCursos() {
